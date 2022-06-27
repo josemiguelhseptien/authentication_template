41f4125d40e3215c0da8df5d03fa9f04857b4c6f
josemiguelhseptien <josemiguelhseptien@gmail.com>
2022-06-27 22:44:19 +0000

    save

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,65 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/login', methods=['POST'])
+def handle_login():
+
+    body = request.json
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if body is None: 
+        return jsonify("Your request is null"), 400
+    if email is None: 
+        return jsonify("Your request is missing an email"), 400
+    if password is None: 
+        return jsonify("Your request is missing a password"), 400
+
+    user = User.query.filter_by(email = email).first()
+    if user is None:
+        return jsonify("User not found"), 404
+
+    if password != user.password:
+        return jsonify("Username and password do not match"), 404
+
+    response_body = {
+        'message': 'its working',
+        'user': user.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def handle_signup():
+
+    body = request.json
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+
+    if body is None: 
+        return jsonify("Your request is null"), 400
+    if email is None: 
+        return jsonify("Your request is missing an email"), 400
+    if password is None: 
+        return jsonify("Your request is missing a password"), 400
+
+    userCheck = User.query.filter_by(email = email).first()
+    if userCheck is not None:
+        return jsonify("email already in use"), 409
+    
+    user = User(email = email, password = password, is_active = True)
+    db.session.add(user)
+    db.session.commit()
+
+
+
+    response_body = {
+        'message': 'its working',
+        'user': user.serialize()
+    }
+
+    return jsonify(response_body), 200
